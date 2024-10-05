59cb47620aedae33d9f878591f530ad854e26d39
xdaviddm <davidmendozadm863@gmail.com>
2024-10-05 10:54:59 -0500

    php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php ?>
+<a href="tipos-datos.php">Tipos-datos</a>
+<a href="variables.php">Variables</a>
+<a href="constantes.php">Constantes</a>
+<a href="arrays.php">Arreglos</a>
+<a href="concantenacion.php">concantenacion</a>
+<a href="operadores_aritmeticos.php">Operadores Aritmeticos</a>
+<a href="asignacion.php">Asignacion</a>
+<a href="asignacion_por_referencia.php">Asignacion por referencia</a>
